d90483079cbe202c565b2bd4fb8b48f88a976214
LTC <tiancheng.li@mail.utoronto.ca>
2024-03-19 22:11:23 +0800

    implemented a simple pytorch dynamic qint8 quantization, it is not available when using GPU, only CPU's time and model size gets better.

--- demo/inference/compress/quantization.py
+++ demo/inference/compress/quantization.py
@@ -1,31 +1,32 @@
 import transformers
 import torch
+import copy
 
-class M(torch.nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        # self.wte = model.transformer.wte
-        # self.wpe = model.transformer.wpe
-        # self.drop = model.transformer.drop
-        self.model = model
-        # QuantStub converts tensors from floating point to quantized
-        self.quant = torch.ao.quantization.QuantStub()
+def _conv1d_to_linear(module):
+    in_size, out_size = module.weight.shape
+    linear = torch.nn.Linear(in_size, out_size)
+    linear.weight.data = module.weight.data.T.contiguous()
+    linear.bias.data = module.bias.data
+    return linear
 
-        # DeQuantStub converts tensors from quantized to floating point
-        self.dequant = torch.ao.quantization.DeQuantStub()
+def conv1d_to_linear(model):
 
-    def forward(self, x):
-        # manually specify where tensors will be converted from floating
-        # point to quantized in the quantized model
-        x = self.quant(x)
-        x = self.model(x)
-        # manually specify where tensors will be converted from quantized
-        # to floating point in the quantized model
-        x = self.dequant(x)
-        return x
+    for name in list(model._modules):
+        if name == "lm_head":
+            continue
+        module = model._modules[name]
+        if isinstance(module, transformers.modeling_utils.Conv1D):
+            linear = _conv1d_to_linear(module)
+            model._modules[name] = linear
+        else:
+            conv1d_to_linear(module)
 
 def apply_quantiztion(model, input):
-    pass
+    #model2 = copy.deepcopy(model)
+    conv1d_to_linear(model)
+    quantized_model = torch.quantization.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
+    model._modules['transformer'] = quantized_model._modules['transformer']
+    return quantized_model
     # model_origin = M(model)
     # model_origin.eval()
     # model_origin.qconfig = torch.quantization.get_default_qconfig("qnnpack")
--- demo/inference/inference.py
+++ demo/inference/inference.py
@@ -5,37 +5,39 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import os
 from track2.demo.inference.compress.prunning import apply_pruning
 from track2.demo.inference.compress.quantization import apply_quantiztion
-
+from track2.demo.inference.compress.knowledge_distillation import apply_knowledge_distillation
 # set mirror for downloading the model
 os.environ["HF_ENDPOINT"] = "http://hf-mirror.com/"
 # set the max number of loops
-STEP_LIMIT = 50
+STEP_LIMIT = 80
 
 # load tokenizer and model online
 # tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 # model = GPT2LMHeadModel.from_pretrained("gpt2", torchscript=True)
-
-# load tokenizer and model offline
-print("loading tokenizer and model")
+model = None
 tokenizer = GPT2Tokenizer.from_pretrained('.././gpt2')
-model = GPT2LMHeadModel.from_pretrained('.././gpt2', torchscript=True)
-
-print("completed loading")
-
-model.eval()
 
 def greet(text, use_pruning, use_quantization, use_knowledge_distillation, use_gpu):
     device = torch.device('cpu')
-    compress_result = compress_model(use_pruning, use_quantization, use_knowledge_distillation)
+
+    # load tokenizer and model offline
+    print("loading model")
+    load_model()
+    print("completed loading")
+
+    # fine-tune
+    compress_result = compress_model(use_pruning, use_quantization, use_knowledge_distillation, use_gpu)
 
     if use_gpu:
         device = torch.device('cuda')
+
+
+
     in_tokens = torch.tensor(tokenizer.encode(text)).to(device)
     out_token = 0
     step = 0
     summary = ""
-    if use_gpu:
-        summary = torchsummary.summary(model, in_tokens)
+    summary = size_check(model)
 
     warm_up(in_tokens)
 
@@ -58,32 +60,57 @@ def greet(text, use_pruning, use_quantization, use_knowledge_distillation, use_g
     # print(prof.table())
     return out_text, prof.table(), summary, compress_result
 
-def compress_model(use_pruning, use_quantization, use_knowledge_distillation):
+def load_model():
+    global model
+    # load tokenizer and model offline
+    print("loading tokenizer and model")
+    tokenizer = GPT2Tokenizer.from_pretrained('.././gpt2')
+    model = GPT2LMHeadModel.from_pretrained('.././gpt2', torchscript=True)
+
+    print("completed loading")
+
+    model.eval()
+
+def compress_model(use_pruning, use_quantization, use_knowledge_distillation, use_gpu):
     # apply 3 different fine-tune technique by modify the model
 
     # logs when applying the techniques
     compress_result = ""
     global model
+    size_1 = size_check(model)
     if use_pruning:
         compress_result += apply_pruning(model)
     if use_quantization:
-        dimmy_input = torch.tensor(tokenizer.encode("hi"))
-        apply_quantiztion(model, dimmy_input)
+        if use_gpu:
+            compress_result += "Cannot use gpu for pytorch qint8 quantization\n"
+        else:
+            dimmy_input = torch.tensor(tokenizer.encode("hi"))
+            apply_quantiztion(model, dimmy_input)
     if use_knowledge_distillation:
-        apply_quantiztion(model)
-
+        apply_knowledge_distillation(model)
+    size_2 = size_check(model)
+    compress_result += size_1 + "->>>>>" + size_2 + "\n"
     return compress_result
 
 def warm_up(in_tokens):
     # Warn-up gpu, cpu
     for _ in range(5):
         start = time.time()
-        outputs = model(in_tokens)
+        model(in_tokens.to(torch.device("cpu")))
         torch.cuda.synchronize()
         end = time.time()
         print('Time:{}ms'.format((end - start) * 1000))
 
+def size_check(model):
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
 
+    size_all_mb = (param_size + buffer_size) / 1024 ** 2
+    return 'model size: {:.3f}MB'.format(size_all_mb)
 
 
 
