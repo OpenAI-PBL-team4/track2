a5ce7a2476c2bd65cd7bf5a8e5a13b16abc0f702
LTC <tiancheng.li@mail.utoronto.ca>
2024-03-22 00:13:55 +0800

    remove old codes in pruning

--- demo/inference/compress/prunning.py
+++ demo/inference/compress/prunning.py
@@ -3,39 +3,12 @@ import transformers
 import torch
 import torch_pruning
 
-class MyMagnitudeImportance(torch_pruning.importance.Importance):
-    pass
 
 def sparsity(name, module):
     return "Sparsity in " + name + ": {:.2f}%".format(
             100. * float(torch.sum(module.weight == 0))
             / float(module.weight.nelement())
         ) + "\n"
-
-def apply_pruning_old(model):
-    prunning_result = ""
-    print("#################Pruning#####################\n")
-    prunning_result += "#################Pruning#####################\n"
-    for name, module in model.named_modules():
-        # prune 20% of connections in all conv1d layers
-        # try:
-        #     prune.l1_unstructured(module, name='weight', amount=0.2)
-        #     print("A pruned", name, type(module))
-        # except:
-        #     pass
-
-        if isinstance(module, transformers.pytorch_utils.Conv1D):
-            prune.l1_unstructured(module, name='weight', amount=0.2)
-            prunning_result += sparsity(name, module)
-
-        # elif isinstance(module, torch.nn.Linear):
-        #     prune.l1_unstructured(module, name='weight', amount=0.4)
-        #     prunning_result += sparsity(name, module)
-
-    prunning_result += "#################Pruned#######################\n"
-    print("#################Pruned#######################\n")
-
-    return prunning_result
 
 
 def apply_pruning(model, example_inputs, pruning_rate, pruning_iteration):
@@ -64,25 +37,11 @@ def apply_pruning(model, example_inputs, pruning_rate, pruning_iteration):
         for group in pruner.step(
                 interactive=True):
             print(group.details())
-            if pruner.DG.check_pruning_group(group):  # 避免将通道剪枝到0
+            if pruner.DG.check_pruning_group(group):
                 group.prune()
         macs, nparams = torch_pruning.utils.count_ops_and_params(model, example_inputs)
         pruning_result += "[Loop "+ str(i) +"]macs:"+ str(macs/1024/1024)+ \
                           "mb,nparams:"+ str(nparams/1024/1024)+ "mb\n"
-
-    # DG = torch_pruning.DependencyGraph()
-    # DG.build_dependency(model, example_inputs=example_inputs)
-    # # group = DG.get_pruning_group(model.transformer.h[0].attn.c_attn,
-    # #                              torch_pruning.prune_linear_out_channels,
-    # #                              idxs=[2, 6, 9])
-    # group = DG.get_pruning_group(model.transformer.wte,
-    #                              torch_pruning.prune_embedding_out_channels,
-    #                              idxs=[2, 6, 9])
-    # for group in DG.get_all_groups(ignored_layers=[model.lm_head], root_module_types=[torch.nn.Embedding, torch.nn.LayerNorm]):
-    #     idxs = [2, 4, 6]  # your pruning indices
-    #     group.prune(idxs=idxs)
-    #     print(group)
-    # return group.__str__()
 
     pruning_result += "#################Pruned#######################\n"
 
